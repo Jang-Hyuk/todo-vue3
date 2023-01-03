825f7d2624418fc38dc3f932dda7e111bd192d71
Jang-Hyuk <wkdgur1380@gmail.com>
2023-01-03 21:16:24 +0900

    초기 대충

---
+++ todo-app/src/views/AboutView.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const state = reactive({
+	count: 20,
+});
+
+function increasement() {
+	state.count += 1;
+}
+</script>
+
+<template>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<div>
+			<button @click="increasement">숫자 추가</button>
+			{{ state.count }}
+		</div>
+	</div>
+</template>
+
+<!-- <script lang="ts">
+export default {
+	setup() {
+		const state = reactive({
+			count: 0,
+		});
+
+		function increasement() {
+			state.count += 1;
+		}
+
+		return { state, increasement };
+	},
+};
+</script> -->
+
+<style>
+@media (min-width: 1024px) {
+	.about {
+		min-height: 100vh;
+		display: flex;
+		align-items: center;
+	}
+}
+</style>
