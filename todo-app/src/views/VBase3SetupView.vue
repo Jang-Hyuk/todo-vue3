0dd4762ec598ebb53f637eb530b0f868ce13f84f
Jang-Hyuk <wkdgur1380@gmail.com>
2023-03-23 00:50:11 +0900

    feat: 테일윈드다 닝겐

--- todo-app/src/views/VBase3SetupView.vue
+++ todo-app/src/views/VBase3SetupView.vue
@@ -1,6 +1,9 @@
 <template>
 	<div>
 		<TodoHeader></TodoHeader>
+		<div class="justify-center flex bg-yellow-300 items-center h-screen">
+			<div class="text-4xl">Hello 👋🏼</div>
+		</div>
 		<button @click="updateStorage">동기화</button>
 		<TodoList></TodoList>
 	</div>
